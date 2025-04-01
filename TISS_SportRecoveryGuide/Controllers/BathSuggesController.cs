c58eb0138afcb262b93a9158aef6875e664f6d21
家齊 潘 <tou522@yahoo.com.tw>
2025-04-01 11:54:34 +0800

    新增選擇方案功能

--- TISS_SportRecoveryGuide/Controllers/BathSuggesController.cs
+++ TISS_SportRecoveryGuide/Controllers/BathSuggesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,17 +13,19 @@ namespace TISS_SportRecoveryGuide.Controllers
         private TISS_SportRecoveryGuideDBEntities _db = new TISS_SportRecoveryGuideDBEntities(); //資料庫
 
         #region 三溫暖泡水建議方案
-        public ActionResult BathSugges()
+        public ActionResult BathSugges(int? conditionId)
         {
-            var conditions = _db.BathCondition.ToList();
             var bathTypes = _db.BathType.OrderBy(bt => bt.BathTypeID).ToList();
+            var allConditions = _db.BathCondition.ToList();
             var map = _db.BathSuggestionMap.ToList();
-            var guidelines = _db.BathUsageGuideline
-                                .OrderBy(g => g.BathTypeID)
-                                .ThenBy(g => g.SortOrder)
-                                .ToList();
+            var guidelines = _db.BathUsageGuideline.OrderBy(g => g.BathTypeID).ThenBy(g => g.SortOrder).ToList();
 
-            var matrix = conditions.Select(condition => new BathSuggestionMatrixViewModel
+            // 篩選條件
+            var selectedConditions = conditionId.HasValue
+                ? allConditions.Where(c => c.ConditionID == conditionId.Value).ToList()
+                : allConditions;
+
+            var matrix = selectedConditions.Select(condition => new BathSuggestionMatrixViewModel
             {
                 ConditionID = condition.ConditionID,
                 Purpose = condition.Purpose,
@@ -30,18 +33,25 @@ namespace TISS_SportRecoveryGuide.Controllers
                 Suggestions = bathTypes.Select(bath =>
                 {
                     var match = map.FirstOrDefault(m => m.ConditionID == condition.ConditionID && m.BathTypeID == bath.BathTypeID);
-
                     return new BathSuggestionItemViewModel
                     {
                         BathTypeID = bath.BathTypeID,
                         BathTypeName = bath.BathTypeName,
-                        IsRecommended = match != null && match.IsRecommended == true
+                        IsRecommended = match != null && match.IsRecommended
                     };
                 }).ToList()
             }).ToList();
 
             ViewBag.BathTypes = bathTypes;
             ViewBag.Guidelines = guidelines;
+            ViewBag.SelectedConditionID = conditionId;
+            ViewBag.AllConditions = allConditions
+                .Select(c => new SelectListItem
+                {
+                    Value = c.ConditionID.ToString(),
+                    Text = $"{c.Purpose} - {c.Timing}"
+                })
+                .ToList();
 
             return View(matrix);
         }
